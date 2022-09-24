17d531ef53ea1109fc2fe3df5d52aeb85ed573ea
lwq <2065277594@qq.com>
2022-09-24 13:33:04 +0800

    1

--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/ServiceVodApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.ggkt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu")//加上他才可以扫描到common中service_utils里的swagger组件
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class,args);
--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -3,11 +3,10 @@ package com.atguigu.ggkt.vod.controller;
 
 import com.atguigu.ggkt.model.vod.Teacher;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,18 +18,29 @@ import java.util.List;
  * @author atguigu
  * @since 2022-09-24
  */
+@Api(tags="讲师管理接口")
 @RestController
 @RequestMapping("/admin/vod/teacher")
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
- 
 
+
+    //逻辑删除
+    @ApiOperation("根据id逻辑删除讲师")
+    @DeleteMapping("remove/{id}")
+    public boolean removeTeacher(@PathVariable Long id){
+        return teacherService.removeById(id);
+    }
+
+    @ApiOperation("查询所有讲师")//swagger注解
     @GetMapping("/findAll")
     public List<Teacher> findAllTeacher(){
         List<Teacher> list = teacherService.list();
         return list;
     }
+
+
 }
 
