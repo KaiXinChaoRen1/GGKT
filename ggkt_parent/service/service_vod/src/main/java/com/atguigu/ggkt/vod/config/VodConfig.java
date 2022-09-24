49ab2e812281c794b3f18a0e5a765cdb7415b5ca
lwq <2065277594@qq.com>
2022-09-24 21:35:00 +0800

    分页插件

--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/config/VodConfig.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/config/VodConfig.java
@@ -1,10 +1,18 @@
 package com.atguigu.ggkt.vod.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.annotations.Cacheable;
 
 @Configuration
 @MapperScan("com/atguigu/ggkt/vod/mapper")
 public class VodConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
 }
