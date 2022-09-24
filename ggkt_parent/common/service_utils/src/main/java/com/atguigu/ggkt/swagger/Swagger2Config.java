ba59fd24dff353783d5e26e84aacb3e5b02eb491
lwq <2065277594@qq.com>
2022-09-24 13:31:49 +0800

    swagger的使用

--- ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/swagger/Swagger2Config.java
+++ ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/swagger/Swagger2Config.java
@@ -11,26 +11,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * 想要在别的模块中使用,需要将本模块引入要用的模块
+ */
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
     @Bean
     public Docket webApiConfig(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("ggkt")
+                .groupName("ggkt组")
                 .apiInfo(webApiInfo())
                 .select()
-                //只显示api路径下的页面
+                //只显示api路径下的页面(注掉不写就代表全部显示)
                 //.paths(Predicates.and(PathSelectors.regex("/api/.*")))
                 .build();
     }
 
     private ApiInfo webApiInfo(){
         return new ApiInfoBuilder()
-                .title("网站-API文档")
-                .description("本文档描述了网站微服务接口定义")
-                .version("1.0")
-                .contact(new Contact("atguigu", "http://atguigu.com", "atguigu.com"))
+                .title("<我是标题>")
+                .description("我是描述..............")
+                .version("我是版本-1.0")
+                .contact(new Contact("天才李文强", "http://hehe.com", "hehe@hehe.com"))
                 .build();
     }
 }
