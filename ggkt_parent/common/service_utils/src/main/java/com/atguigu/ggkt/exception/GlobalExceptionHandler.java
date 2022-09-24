2a5812bdbf395ce21f50f8c82aef5acfdbd4992d
lwq <2065277594@qq.com>
2022-09-24 21:43:12 +0800

    全局异常处理

--- ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
+++ ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice  //aop
+@ControllerAdvice  //属于aop
 public class GlobalExceptionHandler {
 
-    //全局异常处理
-    @ExceptionHandler(Exception.class)
+    //全局异常处理(特定的和自定义的匹配不上,再来找全局的)
+    @ExceptionHandler(Exception.class)//指定异常处理器要处理的异常
     @ResponseBody
     public Result error(Exception e) {
         System.out.println("全局.....");
@@ -17,7 +17,7 @@ public class GlobalExceptionHandler {
         return Result.fail(null).message("执行全局异常处理");
     }
 
-    //特定异常处理ArithmeticException
+    //特定异常处理
     @ExceptionHandler(ArithmeticException.class)
     @ResponseBody
     public Result error(ArithmeticException e) {
@@ -26,7 +26,7 @@ public class GlobalExceptionHandler {
         return Result.fail(null).message("执行ArithmeticException异常处理");
     }
 
-    //自定义异常处理GgktException
+    //自定义异常处理
     @ExceptionHandler(GgktException.class)
     @ResponseBody
     public Result error(GgktException e) {
