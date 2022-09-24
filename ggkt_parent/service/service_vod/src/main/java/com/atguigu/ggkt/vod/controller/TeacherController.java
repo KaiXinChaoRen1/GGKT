c58018fe875269548e2e3a2e8e97ac21d2ff1d90
lwq <2065277594@qq.com>
2022-09-24 21:35:17 +0800

    crud

--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -2,10 +2,16 @@ package com.atguigu.ggkt.vod.controller;
 
 
 import com.atguigu.ggkt.model.vod.Teacher;
+import com.atguigu.ggkt.result.Result;
+import com.atguigu.ggkt.vo.vod.TeacherQueryVo;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +24,107 @@ import java.util.List;
  * @author atguigu
  * @since 2022-09-24
  */
-@Api(tags="讲师管理接口")
+@Api(tags = "讲师管理接口")
 @RestController
 @RequestMapping("/admin/vod/teacher")
+//@CrossOrigin //跨域
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation("条件查询&&分页")
+    @PostMapping("findQueryPage/{current}/{limit}")
+    public Result findPage(@PathVariable long current,
+                           @PathVariable long limit,
+                           @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        //创建page对象
+        Page<Teacher> pageParam = new Page<>(current, limit);
+        //判断teacherQueryVo对象是否为空
+        if (teacherQueryVo == null) {
+            //条件为空,查询全部
+            IPage<Teacher> pageModel = teacherService.page(pageParam, null);
+            return Result.ok(pageModel);
+        } else {
+            //获取条件值，
+            String name = teacherQueryVo.getName();
+            Integer level = teacherQueryVo.getLevel();
+            String joinDateBegin = teacherQueryVo.getJoinDateBegin();
+            String joinDateEnd = teacherQueryVo.getJoinDateEnd();
+            //进行非空判断，条件封装
+            QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+            if (!StringUtils.isEmpty(name)) {
+                wrapper.like("name", name);
+            }
+            if (!StringUtils.isEmpty(level)) {
+                wrapper.eq("level", level);
+            }
+            if (!StringUtils.isEmpty(joinDateBegin)) {
+                wrapper.ge("join_date", joinDateBegin);//大于等于->ge
+            }
+            if (!StringUtils.isEmpty(joinDateEnd)) {
+                wrapper.le("join_date", joinDateEnd);//小于等于->le
+            }
+            //调用方法分页查询
+            IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+            //返回
+            return Result.ok(pageModel);
+        }
+    }
 
-    //逻辑删除
+
+    @ApiOperation("根据id查询")
+    @GetMapping("getTeacher/{id}")
+    public Result getTeacher(@PathVariable Long id) {
+        Teacher teacher = teacherService.getById(id);
+        return Result.ok(teacher);
+    }
+
+    @ApiOperation("修改")
+    @PostMapping("updateTeacher")
+    public Result updateTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.updateById(teacher);
+        if (isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
+    @ApiOperation("添加讲师")
+    @PostMapping("saveTeacher")
+    public Result saveTeacher(@RequestBody Teacher teacher) {
+        boolean isSuccess = teacherService.save(teacher);
+        if (isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
+    @ApiOperation("批量删除讲师")
+    @DeleteMapping("removeBatch")
+    public Result removeBatch(@RequestBody List<Long> idList) {
+        boolean isSuccess = teacherService.removeByIds(idList);
+        if(isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
     @ApiOperation("根据id逻辑删除讲师")
     @DeleteMapping("remove/{id}")
-    public boolean removeTeacher(@PathVariable Long id){
-        return teacherService.removeById(id);
+    public Result removeTeacher(@PathVariable Long id) {
+        boolean b = teacherService.removeById(id);
+        return Result.ok(b).code(999).message("删除了芜湖~~~~");
     }
 
     @ApiOperation("查询所有讲师")//swagger注解
     @GetMapping("/findAll")
-    public List<Teacher> findAllTeacher(){
+    public Result findAllTeacher() {
         List<Teacher> list = teacherService.list();
-        return list;
+        return Result.ok(list);
     }
 
 
