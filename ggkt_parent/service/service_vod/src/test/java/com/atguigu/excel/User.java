196c9bbc022c6a89b21bf83c5eb42940e5f7941a
lwq <2065277594@qq.com>
2022-10-26 21:50:49 +0800

    Excel对应的实体,应该算个vo

--- ggkt_parent/service/service_vod/src/test/java/com/atguigu/excel/User.java
+++ ggkt_parent/service/service_vod/src/test/java/com/atguigu/excel/User.java
@@ -3,12 +3,16 @@ package com.atguigu.excel;
 import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
+/**
+ * Excel对应的实体,应该算个vo
+ */
+
 @Data
 public class User {
 
-    @ExcelProperty(value = "用户编号",index = 0)    //表头and对应的列数
+    @ExcelProperty(value = "用户编号", index = 0)    //表头and对应的列数
     private int id;
 
-    @ExcelProperty(value = "用户名称",index = 1)
+    @ExcelProperty(value = "用户名称", index = 1)
     private String name;
 }
