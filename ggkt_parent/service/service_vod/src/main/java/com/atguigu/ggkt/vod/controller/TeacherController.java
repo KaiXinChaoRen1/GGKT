e230d3438fc676f7a4eb77ade960dcd97b72a585
lwq <2065277594@qq.com>
2022-10-30 21:55:34 +0800

    注释Controller的跨域,由gateway统一处理

--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -27,7 +27,7 @@ import java.util.List;
 @Api(tags = "讲师管理接口")
 @RestController
 @RequestMapping("/admin/vod/teacher")
-@CrossOrigin //跨域
+//@CrossOrigin //跨域
 public class TeacherController {
 
     @Autowired
--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/VideoVisitorController.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/VideoVisitorController.java
@@ -17,7 +17,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping(value = "/admin/vod/videoVisitor")
-@CrossOrigin
+//@CrossOrigin
 public class VideoVisitorController {
 
     @Autowired
