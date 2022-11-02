56ceed42de5a8d95a68e6f512690c5325e676f38
lwq <2065277594@qq.com>
2022-11-02 21:47:17 +0800

    用户信息接口远程调用的接口声明

--- ggkt_parent/service_client/service_user_client/src/main/java/com/atguigu/ggkt/client/user/UserInfoFeignClient.java
+++ ggkt_parent/service_client/service_user_client/src/main/java/com/atguigu/ggkt/client/user/UserInfoFeignClient.java
@@ -5,9 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "service-user")
+/**
+ *  编写接口专门提供远程调用
+ */
+
+@FeignClient(value = "service-user") //接口所在的服务名字
 public interface UserInfoFeignClient {
 
-    @GetMapping("/admin/user/userInfo/inner/getById/{id}")
+    @GetMapping("/admin/user/userInfo/inner/getById/{id}")  //路径要写全
     public UserInfo getById(@PathVariable Long id);
 }
