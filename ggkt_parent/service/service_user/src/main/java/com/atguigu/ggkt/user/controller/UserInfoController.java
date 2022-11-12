48087b2e5b2ba667d11a3a5c228f5799074378e9
lwq <2065277594@qq.com>
2022-11-12 19:05:59 +0800

    获取用户详细信息的方法

--- ggkt_parent/service/service_user/src/main/java/com/atguigu/ggkt/user/controller/UserInfoController.java
+++ ggkt_parent/service/service_user/src/main/java/com/atguigu/ggkt/user/controller/UserInfoController.java
@@ -12,12 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- * 用户表 前端控制器
- * </p>
- *
- * @author atguigu
- * @since 2022-04-29
+ * 获取用户的详细信息
  */
 @RestController
 @RequestMapping("/admin/user/userInfo")
